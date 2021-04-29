8ab84385894315e9f883738ed42b7dcbcdd80ca9
Umang <umangagarwal345@gmail.com>
2021-04-29 22:29:49 +0530

    Next Larger Element

---
+++ src/com/example/test/DataStructures/Stacks/NextGreaterElement.java
@@ -0,0 +1,58 @@
+package com.example.test.DataStructures.Stacks;
+
+import java.util.Stack;
+
+//    Given an array arr[ ] of size N having distinct elements, the task is to find the next greater element
+//    for each element of the array in order of their appearance in the array.
+//    Next greater element of an element in the array is the nearest element on the right which is greater
+//    than the current element.
+//    If there does not exist next greater of current element, then next greater element for current element is -1.
+//    For example, next greater of the last element is always -1.
+
+public class NextGreaterElement
+{
+    public static void main(String[] args) {
+         long[] in = {5,7,2,5,1,8,9};
+         long[] result = findNextLargerElement(in,7);
+         for(long x : result)
+         {
+             System.out.print(x+" ");
+         }
+        System.out.println();
+    }
+    public static long[] findNextLargerElement(long[] arr, int n)
+    {
+        long[] ans = new long[n];
+        Stack<Long> stack = new Stack<Long>();
+        int k=n-1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(stack.isEmpty())
+            {
+                ans[k--] = -1;
+            }
+            else if(stack.peek()>arr[i])
+            {
+                ans[k--] = stack.peek();
+            }
+            else
+            {
+                while(!stack.isEmpty() && stack.peek()<=arr[i])
+                {
+                    stack.pop();
+                }
+                if(stack.isEmpty())
+                {
+                    ans[k--] = -1;
+                }
+                else
+                {
+                    ans[k--] = stack.peek();
+                }
+            }
+            stack.push(arr[i]);
+        }
+
+        return ans;
+    }
+}
